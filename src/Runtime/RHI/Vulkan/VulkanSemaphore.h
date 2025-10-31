e8105a3bda4c22e4fb3ae5e8f13f8837ab14726b
Steelwall2014 <465964358@qq.com>
2025-10-31 18:09:53 +0800

    修复bug

--- src/Runtime/RHI/Vulkan/VulkanSemaphore.h
+++ src/Runtime/RHI/Vulkan/VulkanSemaphore.h
@@ -4,30 +4,6 @@
 
 namespace nilou {
 
-class FVulkanSemaphore
-{
-public:
-    FVulkanSemaphore()
-        : Device(VK_NULL_HANDLE)
-    {
-    }
-    FVulkanSemaphore(VkDevice InDevice)
-        : Device(InDevice)
-    {
-        VkSemaphoreCreateInfo info{};
-        info.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
-        vkCreateSemaphore(Device, &info, nullptr, &Handle);
-    }
-    ~FVulkanSemaphore()
-    {
-        if (Device && Handle)
-            vkDestroySemaphore(Device, Handle, nullptr);
-    }
-    VkSemaphore Handle{};
-
-    VkDevice Device{};
-};
-
 class VulkanSemaphore : public RHISemaphore 
 {
 public:
