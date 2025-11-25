3aee0e303ed8c7fdabacc608ac5b142310677bd0
Steelwall2014 <465964358@qq.com>
2025-11-25 20:18:35 +0800

    新增slang shading language库

--- Engine/Source/ThirdParty/vulkan/xmake.lua
+++
@@ -1,6 +0,0 @@
--- target("VulkanSDK")
---     set_kind("headeronly")
---     add_includedirs("$(env VK_SDK_PATH)/Include", {public = true})
---     add_links("$(env VK_SDK_PATH)/Lib/vulkan-1", {public = true})
---     add_links("$(env VK_SDK_PATH)/Lib/shaderc*", {public = true})
---     add_links("$(env VK_SDK_PATH)/Lib/glslang", {public = true})
